91dd10c3d806ecaae8f8d961d097113be0b9062a
José David Vela Castro <josedavidvela@gmail.com>
2022-10-14 19:32:47 +0200

    Add files via upload

---
+++ precache-manifest.bf443538279bedfa44b6a5eaeeca99fb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "da490707f89badcb677528f81bb2a01d",
+    "url": "assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "525f9e942393ccab141d",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "4a6d6a659c67d2ec5eeff15cf12566d6",
+    "url": "vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "e044a63e034bf10304dad73138b8c74b",
+    "url": "vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "47d6162abb84f5f98b5913a78a6d40b4",
+    "url": "vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "16a6e231db0f767f82644d8cfa072ba0",
+    "url": "vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "e4803801395c2d2dce6462bf75808b3a",
+    "url": "vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "fd3b6605f07e1064b91fe11db4b62cc3",
+    "url": "vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "cd57885363cc2415b8016631bcb6f582",
+    "url": "index.html"
+  },
+  {
+    "revision": "47a8e4f6c36fb20500af0bc5542cc483",
+    "url": "assets/logo.svg"
+  },
+  {
+    "revision": "1405359f10118373dfa8a553f6ddb6be",
+    "url": "assets/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "587a80ff170239b1754de3a3ae399c03",
+    "url": "assets/icons/polymer-skeleton-96x96.png"
+  },
+  {
+    "revision": "57c78e0130446eb06e495edb54e85f6b",
+    "url": "assets/icons/polymer-skeleton-48x48.png"
+  },
+  {
+    "revision": "b06c7fa69524acb9784753237a82ae99",
+    "url": "assets/icons/polymer-skeleton-384x384.png"
+  },
+  {
+    "revision": "1a4091319f83de18a8093e9b37e47875",
+    "url": "assets/icons/polymer-skeleton-192x192.png"
+  },
+  {
+    "revision": "15f15453a7d68d404b02a319cfd9e6b1",
+    "url": "assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "490efe304d7b8c82275b847fb4d6a8d9",
+    "url": "assets/icons/browserconfig.xml"
+  },
+  {
+    "revision": "3c85e2cf65b61a2ff0aafdde9f909e9a",
+    "url": "assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "70b3dcab6ca5705e2a6b662a807d0ee6",
+    "url": "assets/AnimatedSkeleton.svg"
+  }
+];
